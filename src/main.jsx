3f96861394bd2435318165d3dac05ae7f76f186c
MatheusCosta616 <rm551157@fiap.com.br>
2023-11-18 17:50:14 -0300

    first commit

---
+++ src/main.jsx
@@ -0,0 +1,23 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import App from './App.jsx'
+import './index.css'
+import Home from './routes/Home.jsx'
+
+import { createBrowserRouter,RouterProvider } from 'react-router-dom';
+import Login from './routes/Login.jsx'
+
+const router = createBrowserRouter([
+  {path: "/", element: <App/>,
+    children:[
+      { path:"/", element:<Login/> },
+      { path:"/home", element:<Home />},
+    ]    
+  }
+]);
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <RouterProvider router={router}/>
+  </React.StrictMode>,
+)
